ce2eddfca05ce20f8c77f03f95a02ddcc156805d
Subesh Sapkota <subeshsapkota@gmail.com>
2020-06-08 23:11:47 +0530

    Update server.js

--- Angular6FirebaseCRUD/server.js
+++ Angular6FirebaseCRUD/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/milkorderonline'));
+app.use(express.static(__dirname + '/dist/angular6-firebase-crud'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/milkorderonline/index.html'));});
+'/dist/angular6-firebase-crud/index.html'));});
 app.listen(process.env.PORT || 8080);
