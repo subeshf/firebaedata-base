cbb6eae15254d9fc79f4c7e4b4012d80c7c2d676
Subesh Sapkota <subeshsapkota@gmail.com>
2020-06-08 23:13:23 +0530

    Update environment.ts

--- Angular6FirebaseCRUD/src/environments/environment.ts
+++ Angular6FirebaseCRUD/src/environments/environment.ts
@@ -1,23 +1,26 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCOBbT2J7Rvt-YH_Doh9P3TdTHJj1Hjk5o",
-    authDomain: "fir-crudapp-8a322.firebaseapp.com",
-    databaseURL: "https://fir-crudapp-8a322.firebaseio.com",
-    projectId: "fir-crudapp-8a322",
-    storageBucket: "fir-crudapp-8a322.appspot.com",
-    messagingSenderId: "931022078246"
+    apiKey: "AIzaSyAsrd--ngSBdqVnFBGs4FSbqb2ml4bQ-5g",
+    authDomain: "milkpatna-389f6.firebaseapp.com",
+    databaseURL: "https://milkpatna-389f6.firebaseio.com",
+    projectId: "milkpatna-389f6",
+    storageBucket: "milkpatna-389f6.appspot.com",
+    messagingSenderId: "970143758066",
+    appId: "1:970143758066:web:990e07f3aedf7070b3426b",
+    measurementId: "G-61VN3ZBWJ2"
   }
 };
 
 /*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
